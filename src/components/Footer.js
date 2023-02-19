c5a4c2f9ff663459aad7fdf113ad94bc860a3250
Ognen Veljanovski <109540795+ognemavl@users.noreply.github.com>
2023-02-19 19:47:25 +0100

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import Logo from "../assets/img/Logo.png";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={Logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/ognen-veljanovski-470069238/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/profile.php?id=100089268004045"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://instagram.com/official.ognen?igshid=YmMyMTA2M2Y="><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2023. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
